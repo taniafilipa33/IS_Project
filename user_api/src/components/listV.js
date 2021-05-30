221bff5cd65aa197176ff5f5c058e2ce7bc5f5a3
taniafilipa33 <com.tani33@gmail.com>
2021-05-30 20:27:16 +0100

    ascending bug

--- user_api/src/components/listV.js
+++ user_api/src/components/listV.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 
 import CRUDTable, {
   Fields,
@@ -11,149 +11,182 @@ import CRUDTable, {
 // Component's Base CSS
 import "../style/index.css";
 
-export default function ListComposition(props) {
-  console.log("entrei");
-  const { id } = props.match.params;
-  console.log(id);
-  const DescriptionRenderer = ({ field }) => <textarea {...field} />;
-  //fetch("http://localhost:7300/ehr/" + id + "/versioned")
-  //  .then((response) => response.json())
-  //  .then((data) => {
-  //    let tasks = data;
-  let tasks = [];
-  const SORTERS = {
-    NUMBER_ASCENDING: (mapper) => (a, b) => mapper(a) - mapper(b),
-    NUMBER_DESCENDING: (mapper) => (a, b) => mapper(b) - mapper(a),
-    STRING_ASCENDING: (mapper) => (a, b) => mapper(a).localeCompare(mapper(b)),
-    STRING_DESCENDING: (mapper) => (a, b) => mapper(b).localeCompare(mapper(a)),
-  };
+class ListVersioned extends Component {
+  constructor(props) {
+    super(props);
+    this.count = 0;
+    this.state = { tasks: [], isLoading: true };
+    this.id = props.match.params;
+    this.DescriptionRenderer = ({ field }) => <textarea {...field} />;
+    this.idEhr = "";
 
-  const getSorter = (data) => {
+    this.styles = {
+      container: { margin: "auto", width: "fit-content" },
+    };
+  }
+
+  getSorter(data) {
     const mapper = (x) => x[data.field];
-    let sorter = SORTERS.STRING_ASCENDING(mapper);
+    let sorter = this.SORTERS.STRING_ASCENDING(mapper);
 
     if (data.field === "id") {
       sorter =
         data.direction === "ascending"
-          ? SORTERS.NUMBER_ASCENDING(mapper)
-          : SORTERS.NUMBER_DESCENDING(mapper);
+          ? this.SORTERS.NUMBER_ASCENDING(mapper)
+          : this.SORTERS.NUMBER_DESCENDING(mapper);
     } else {
       sorter =
         data.direction === "ascending"
-          ? SORTERS.STRING_ASCENDING(mapper)
-          : SORTERS.STRING_DESCENDING(mapper);
+          ? this.SORTERS.STRING_ASCENDING(mapper)
+          : this.SORTERS.STRING_DESCENDING(mapper);
     }
-
     return sorter;
-  };
+  }
 
-  let count = tasks.length;
-  const service = {
-    fetchItems: (payload) => {
-      let result = Array.from(tasks);
-      result = result.sort(getSorter(payload.sort));
-      return Promise.resolve(result);
-    },
-    create: (task) => {
-      count += 1;
-      tasks.push({
-        ...task,
-        id: count,
+  callAPI() {
+    fetch("http://localhost:7300/ehr/" + this.id + "/versioned")
+      .then((res) => res.text())
+      .then((res) => {
+        var resp = [];
+        //console.log(JSON.parse(res));
+        JSON.parse(res).map((ele) => {
+          resp.push(ele);
+        });
+        this.setState({ tasks: resp });
+        this.count = this.state.tasks.length;
+        console.log(this.state.tasks);
+        this.SORTERS = {
+          NUMBER_ASCENDING: (mapper) => (a, b) => mapper(a) - mapper(b),
+          NUMBER_DESCENDING: (mapper) => (a, b) => mapper(b) - mapper(a),
+          STRING_ASCENDING: (mapper) => (a, b) =>
+            mapper(a).localeCompare(mapper(b)),
+          STRING_DESCENDING: (mapper) => (a, b) =>
+            mapper(b).localeCompare(mapper(a)),
+        };
+        this.service = {
+          fetchItems: (payload) => {
+            let result = Array.from(this.state.tasks);
+            console.log("dentro do fetchItems" + this.state.tasks);
+            result = result.sort(this.getSorter(payload.sort));
+            return Promise.resolve(result);
+          },
+          create: (task) => {
+            this.count += 1;
+            this.state.tasks.push({
+              ...task,
+              id: this.count,
+            });
+            return Promise.resolve(task);
+          },
+          update: (data) => {
+            const task = this.state.tasks.find((t) => t.id === data.id);
+            task.title = data.title;
+            task.description = data.description;
+            return Promise.resolve(task);
+          },
+          delete: (data) => {
+            const task = this.state.tasks.find((t) => t.id === data.id);
+            this.state.tasks = this.state.tasks.filter((t) => t.id !== task.id);
+            return Promise.resolve(task);
+          },
+        };
+
+        this.setState({ isLoading: false });
       });
-      return Promise.resolve(task);
-    },
-    update: (data) => {
-      const task = tasks.find((t) => t.id === data.id);
-      task.title = data.title;
-      task.description = data.description;
-      return Promise.resolve(task);
-    },
-    delete: (data) => {
-      const task = tasks.find((t) => t.id === data.id);
-      tasks = tasks.filter((t) => t.id !== task.id);
-      return Promise.resolve(task);
-    },
-  };
+  }
 
-  const styles = {
-    container: { margin: "auto", width: "fit-content" },
-  };
+  componentDidMount() {
+    this.callAPI();
+  }
 
-  return (
-    <div style={styles.container}>
-      <CRUDTable
-        caption="Tasks"
-        fetchItems={(payload) => service.fetchItems(payload)}
-      >
-        <Fields>
-          <Field name="id" label="Id" hideInCreateForm readOnly />
-          <Field name="title" label="Title" placeholder="Title" />
-          <Field
-            name="description"
-            label="Description"
-            render={DescriptionRenderer}
-          />
-        </Fields>
-        <CreateForm
-          title="Task Creation"
-          message="Create a new task!"
-          trigger="Create Task"
-          onSubmit={(task) => service.create(task)}
-          submitText="Create"
-          validate={(values) => {
-            const errors = {};
-            if (!values.title) {
-              errors.title = "Please, provide task's title";
-            }
+  render() {
+    if (this.state.isLoading === true) {
+      return (
+        <div>
+          <h1>login...</h1>
+        </div>
+      );
+    } else {
+      return (
+        <div style={this.styles.container}>
+          <CRUDTable
+            caption="Versioned Compositions"
+            fetchItems={(payload) => this.service.fetchItems(payload)}
+          >
+            <Fields>
+              <Field name="uid" label="Id" hideInCreateForm readOnly />
+              <Field name="type" label="Type" placeholder="Type" />
+              <Field
+                name="owner_id"
+                label="Owner Type"
+                render={this.DescriptionRenderer}
+              />
+            </Fields>
+            <CreateForm
+              title="Versioned Creation"
+              message="Start a new Record"
+              trigger="Create Versioned Composition"
+              onSubmit={(task) => this.service.create(task)}
+              submitText="Create"
+              validate={(values) => {
+                const errors = {};
+                values.owner_id = this.id;
+                if (!values.title) {
+                  errors.title = "Please, provide task's title";
+                }
 
-            if (!values.description) {
-              errors.description = "Please, provide task's description";
-            }
+                if (!values.description) {
+                  errors.description = "Please, provide task's description";
+                }
 
-            return errors;
-          }}
-        />
+                return errors;
+              }}
+            />
 
-        <UpdateForm
-          title="Task Update Process"
-          message="Update task"
-          trigger="Update"
-          onSubmit={(task) => service.update(task)}
-          submitText="Update"
-          validate={(values) => {
-            const errors = {};
+            <UpdateForm
+              title="Task Update Process"
+              message="Update task"
+              trigger="Update"
+              onSubmit={(task) => this.service.update(task)}
+              submitText="Update"
+              validate={(values) => {
+                const errors = {};
 
-            if (!values.id) {
-              errors.id = "Please, provide id";
-            }
+                if (!values.id) {
+                  errors.id = "Please, provide id";
+                }
 
-            if (!values.title) {
-              errors.title = "Please, provide task's title";
-            }
+                if (!values.title) {
+                  errors.title = "Please, provide task's title";
+                }
 
-            if (!values.description) {
-              errors.description = "Please, provide task's description";
-            }
+                if (!values.description) {
+                  errors.description = "Please, provide task's description";
+                }
 
-            return errors;
-          }}
-        />
+                return errors;
+              }}
+            />
 
-        <DeleteForm
-          title="Task Delete Process"
-          message="Are you sure you want to delete the task?"
-          trigger="Delete"
-          onSubmit={(task) => service.delete(task)}
-          submitText="Delete"
-          validate={(values) => {
-            const errors = {};
-            if (!values.id) {
-              errors.id = "Please, provide id";
-            }
-            return errors;
-          }}
-        />
-      </CRUDTable>
-    </div>
-  );
+            <DeleteForm
+              title="Task Delete Process"
+              message="Are you sure you want to delete the task?"
+              trigger="Delete"
+              onSubmit={(task) => this.service.delete(task)}
+              submitText="Delete"
+              validate={(values) => {
+                const errors = {};
+                if (!values.id) {
+                  errors.id = "Please, provide id";
+                }
+                return errors;
+              }}
+            />
+          </CRUDTable>
+        </div>
+      );
+    }
+  }
 }
+
+export default ListVersioned;
