23cecdf61540a06a7bf2ac8dc3ba503e43855279
Margarida Campos <margarida.maia.campos@gmail.com>
2021-06-13 11:47:58 +0100

    handle click

--- user_api/src/components/Info.js
+++ user_api/src/components/Info.js
@@ -29,6 +29,13 @@ class Info extends Component {
       });
   }
 
+  handleClick = (e) => {
+    console.log(e.target.id)
+    console.log("target==",e)
+    this.setState({tasks: e.target.value})
+    console.log("target==",e.target.value)
+  }
+
   componentDidMount() {
     this.callAPI();
   }
@@ -102,6 +109,7 @@ class Info extends Component {
                                             type="text"
                                             className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                                             value={valor[1]}
+                                            onChange= {this.handleClick}
                                           />
                                         </div>
                                       </div>
@@ -115,6 +123,7 @@ class Info extends Component {
                             <button
                               className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
                               type="button"
+                             
                             >
                               Save
                             </button>
